37bda907146b68b1db9c20f59005b22d964bbf1f
root <root@7f3c4ae3fc3e>
2017-05-04 19:32:56 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "autumn",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Alberstons",
+  "homePage" : "Main",
+  "name" : "Alberstons",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
